127a69142b594996a7497a0f1b468614e1713c48
Leo Buck <leonardo@timais.com>
2021-01-10 10:43:34 -0300

    Capítulo 4. Melhorando o cadastro e a listagem

--- src/main/java/org/casadocodigo/loja/config/JPAConfiguration.java
+++ src/main/java/org/casadocodigo/loja/config/JPAConfiguration.java
@@ -2,14 +2,19 @@ package org.casadocodigo.loja.config;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 public class JPAConfiguration {
 
 	@Bean
@@ -30,7 +35,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setPassword("");
 		return dataSource;
 	}
 	
@@ -40,5 +45,12 @@ public class JPAConfiguration {
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.setProperty("hibernate.show_sql", "true");
 		return properties;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
 	}
 }
---
+++ src/main/java/org/casadocodigo/loja/models/Price.java
@@ -0,0 +1,32 @@
+package org.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+
+	@Column(scale = 2)
+	private BigDecimal value;
+	
+	private BookType bookType;
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public BookType getBookType() {
+		return bookType;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public void setBookType(BookType bookType) {
+		this.bookType = bookType;
+	}
+	
+}
